231e3c83a222675ee5320cfec932faa5f1007c95
기웅민 <kiwoongmin@giungmin-ui-MacBookPro.local>
2022-05-22 20:55:54 +0900

    refactor: 반응형, api 컴포넌트 분리

--- src/components/DetailPage/DetailPage.tsx
+++ src/components/DetailPage/DetailPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import styled from '@emotion/styled';
 import { optionLists } from 'app/App';
 
 export default function DetailPage({ lists }: { lists: optionLists[] }) {
@@ -9,15 +10,37 @@ export default function DetailPage({ lists }: { lists: optionLists[] }) {
   return (
     <>
       {item &&
-      <>
-        <img src={ item.club.coverUrl } alt='메인 이미지' />
-        <h2>{ item.club.name }</h2>
-        <p>클럽 유형: { item.club.type }</p>
-        <p>장소: { item.club.place }</p>
-        <p>가격: { item.price }원</p>
-        <p>{ item.club.description }</p>
-      </>
+        <DetailWrapper>
+          <TitleDiv>상세 페이지</TitleDiv>
+          <img src={ item.club.coverUrl } alt='메인 이미지' />
+          <h2>제목: { item.club.name }</h2>
+          <p>클럽 유형: { item.club.type }</p>
+          <p>장소: { item.club.place }</p>
+          <p>가격: { item.price }원</p>
+          <p>{ item.club.description }</p>
+        </DetailWrapper>
       }
     </>
-  )
+  );
 }
+
+const DetailWrapper = styled('div')`
+  width: 100%;
+  height: 100vh;
+  margin-top: 1rem;
+  border: 2px solid lightcoral;
+  justify-content: center;
+  text-align: center;
+
+  img {
+    width: 50%;
+    height: 50%;
+  }
+`;
+
+const TitleDiv = styled('div')`
+  margin: 1rem 0 1rem 0;
+  font-size: 30px;
+  font-weight: bold;
+  color: lightcoral;
+`;
